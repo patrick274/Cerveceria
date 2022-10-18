ed1545e71f1f030764c61065035ed33c0bbe3b26
maxigallardo1 <maxi_g_2001@hotmail.com>
2022-10-18 16:11:32 -0300

    agregue un archivo js e hice un par de cambios en el index
    a

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+var btnAbrirPopup = document.getElementById('btn-contacto'),
+	overlay = document.getElementById("overlay"),
+	popup = document.getElementById("popup"),
+	btnCerrarPopup = document.getElementById("btn-cerrar-popup");
+   
+    btnAbrirPopup.addEventListener("click",overlay.classList.add("active"));
+
